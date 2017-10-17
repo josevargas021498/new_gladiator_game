e680cfa84194c600b3a9fa170864bd1b4f0f3937
Jose Vargas <jvargas@basecampcodingacademy.org>
2017-10-17 16:27:01 -0500

    done, fully functional

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,7 +3,8 @@ const other = require('../../lib/other');
 const appRoot = $('#app');
 
 const STATE = {
-    updates: ''
+    updates: '',
+    turn: 1
 };
 
 function createGladiator(name, health, rage) {
@@ -14,21 +15,70 @@ function createGladiator(name, health, rage) {
     return gladiator;
 }
 
-var gladiator_1 = createGladiator('Jose', 200, 30);
-var gladiator_2 = createGladiator('Liza', 200, 30);
+function viewG1Buttons() {
+    if (STATE.turn === 1) {
+        return [
+            '<h2>' + gladiator_1.name + ", it's your turn! </h2>",
+            "<div><button id='kick' > Kick! </button></div>",
+            "<div><button id='punch'> Punch! </button></div>",
+            "<div><button id='conserve'> Conserve! </button></div>",
+            "<div><button id='heal'> Heal! </button></div>",
+            STATE.updates
+        ].join('');
+    } else {
+        return [
+            "<div><button disabled id='kick' > Kick! </button></div>",
+            "<div><button disabled id='punch'> Punch! </button></div>",
+            "<div><button disabled id-'conserve'> Conserve! </button></div>",
+            "<div><button disabled id='heal'> Heal! </button></div>",
+            STATE.updates
+        ].join('');
+    }
+}
 
-function viewButtons() {
+function viewG2Buttons() {
+    if (STATE.turn === 2) {
+        return [
+            '<h2>' + gladiator_2.name + ", it's your turn! </h2>",
+            "<div><button id='kick2'> Kick! </button></div>",
+            "<div><button id='punch2'> Punch! </button></div>",
+            "<div><button id='conserve2'> Conserve! </button></div>",
+            "<div><button id='heal2'> Heal! </button></div>",
+            STATE.updates
+        ].join('');
+    } else {
+        return [
+            "<div><button disabled id='kick2'> Kick! </button></div>",
+            "<div><button disabled id='punch2'> Punch! </button></div>",
+            "<div><button disabled id='conserve2'> Conserve! </button></div>",
+            "<div><button disabled id='heal2'> Heal! </button></div>",
+            STATE.updates
+        ].join('');
+    }
+}
+
+var gladiator_1 = createGladiator('Jose', 100, 30);
+var gladiator_2 = createGladiator('Liza', 100, 30);
+
+function viewGladiator1() {
     return [
+        '<div class="col-lg-6">',
+        '<h3>' + 'Name: ' + gladiator_1.name + '</h3>',
+        '<h3>' + 'Health: ' + gladiator_1.health + '</h3>',
+        '<H3>' + 'Rage: ' + gladiator_1.rage + '</h3></br>',
+        viewG1Buttons(),
+        '</div>'
+    ].join('');
+}
+
+function viewGladiator2() {
+    return [
+        '<div class="col-lg-6">',
         '<h3>' + 'Name: ' + gladiator_2.name + '</h3>',
         '<h3>' + 'Health: ' + gladiator_2.health + '</h3>',
         '<h3>' + 'Rage: ' + gladiator_2.rage + '</h3></br>',
-        '<h3>' + 'Name: ' + gladiator_1.name + '</h3>',
-        '<h3>' + 'Health: ' + gladiator_1.health + '</h3>',
-        '<H3>' + 'Rage: ' + gladiator_1.rage + '</h3>',
-        "<div><button id='kick'> Kick! </button></div>",
-        "<div><button id='punch'> Punch! </button></div>",
-        "<div><button id='heal'> Heal! </button></div>",
-        STATE.updates
+        viewG2Buttons(),
+        '</div>'
     ].join('');
 }
 
@@ -41,6 +91,19 @@ function buttonAttHandler() {
             '   --- 45 Health       //      ' +
             gladiator_1.name +
             '   +++25 Rage ';
+        STATE.turn = 2;
+
+        draw();
+    });
+    $('#kick2').click(function() {
+        gladiator_2.rage += 25;
+        gladiator_1.health -= 45;
+        STATE.updates =
+            gladiator_1.name +
+            '   --- 45 Health       //      ' +
+            gladiator_2.name +
+            '   +++25 Rage ';
+        STATE.turn = 1;
         draw();
     });
     $('#punch').click(function() {
@@ -51,6 +114,18 @@ function buttonAttHandler() {
             '   ---30 Health' +
             gladiator_1.name +
             '+++ 15 Rage    ';
+        STATE.turn = 2;
+        draw();
+    });
+    $('#punch2').click(function() {
+        gladiator_2.rage += 15;
+        gladiator_1.health -= 26;
+        STATE.updates =
+            gladiator_1.name +
+            '   ---26 Health' +
+            gladiator_2.name +
+            '+++ 15 Rage    ';
+        STATE.turn = 1;
         draw();
     });
     $('#heal').click(function() {
@@ -61,13 +136,48 @@ function buttonAttHandler() {
             '    +++ 10 Health //   ' +
             gladiator_1.name +
             '  --- 5 Rage   ';
+        STATE.turn = 2;
+        draw();
+    });
+    $('#heal2').click(function() {
+        gladiator_2.health += 10;
+        gladiator_2.rage -= 5;
+        STATE.updates =
+            gladiator_2.name +
+            '    +++ 10 Health //   ' +
+            gladiator_2.name +
+            '  --- 5 Rage   ';
+        STATE.turn = 1;
         draw();
     });
 }
 
+function G1IsDead() {
+    appRoot.html(
+        '<button onclick="document.location.reload()">' +
+            gladiator_1.name +
+            ', YOU DIED! </br>Click Here To Restart! </button>'
+    );
+}
+
+function G2IsDead() {
+    appRoot.html(
+        '<button onclick="document.location.reload()">' +
+            gladiator_2.name +
+            ', YOU DIED! </br>Click Here To Restart! </button>'
+    );
+}
+function isDead() {
+    if (gladiator_1.health <= 0) {
+        G1IsDead();
+    } else if (gladiator_2.health <= 0) {
+        G2IsDead();
+    }
+}
 function draw() {
-    appRoot.html(viewButtons());
+    appRoot.html(viewGladiator1() + viewGladiator2());
     buttonAttHandler();
+    isDead();
 }
 function main() {
     draw();
